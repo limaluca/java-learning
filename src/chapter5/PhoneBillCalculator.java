4048c5b21b457377c9d4645bd095ca09047dbf29
limaluca <lucaslimamesmo@gmail.com>
2021-02-12 15:08:37 -0300

    Methods practice: Build a program that calculates a phone bill with taxes.

---
+++ src/chapter5/PhoneBillCalculator.java
@@ -0,0 +1,56 @@
+package chapter5;
+import java.util.Scanner;
+
+public class PhoneBillCalculator {
+    static double overageCharge = 0.25;
+    static double tax = 0.15;
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String args[]){
+
+        double plan = getPlanCost();
+        int minutes = getOverageMinutes();
+        double minutesFee = chargeMinutes(minutes);
+        double taxes = chargeTax(plan,minutesFee);
+        double total = getTotal(plan,minutesFee,taxes);
+        getBill(plan,minutesFee,taxes,total);
+
+    }
+
+    public static double getPlanCost(){
+        System.out.println("Please, inform the cost of  your plan");
+        double cost = input.nextDouble();
+        return cost;
+    }
+
+    public static int getOverageMinutes(){
+        System.out.println("Now, inform the number of overage minutes: ");
+        int minutes = input.nextInt();
+        return minutes;
+    }
+
+    public static double chargeMinutes(int minutes){
+        double fee = minutes * overageCharge;
+        return fee;
+    }
+
+    public static double chargeTax(double planCost, double overageCharge){
+        double fee = (planCost + overageCharge) * tax;
+        return fee;
+    }
+
+    public static double getTotal(double planCost, double minutesCharge, double taxCharge){
+        return planCost + minutesCharge + taxCharge;
+    }
+
+    public static void getBill(double planCost, double minutesCharge, double taxCharge, double total){
+        System.out.println("Detailed phone bill");
+        System.out.println("Plan cost: " + planCost);
+        System.out.println("Overage cost: " + minutesCharge);
+        System.out.printf("Taxes: %.2f\n",taxCharge);
+        System.out.printf("Total: %.2f", total);
+
+    }
+
+
+}
